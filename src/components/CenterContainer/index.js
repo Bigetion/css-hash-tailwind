29fa4b41032577c34838459c0be6d41ca0028ea2
Bigetion <adysuprianto@yahoo.co.id>
2020-09-27 16:03:34 +0700

    Update Center Container

--- src/components/CenterContainer/index.js
+++ src/components/CenterContainer/index.js
@@ -15,14 +15,48 @@ const centerContainerClass = cssHash(
   `
 );
 
+const centerXContainerClass = cssHash(
+  (className) => `
+    .${className} {
+      position: relative;
+    }
+    .${className} > :first-child {
+      position: absolute;
+      top: 50%;
+      left: 0%;
+      transform: translate(-0%, -50%);
+    }
+  `
+);
+
+const centerYContainerClass = cssHash(
+  (className) => `
+    .${className} {
+      position: relative;
+    }
+    .${className} > :first-child {
+      position: absolute;
+      top: 0%;
+      left: 50%;
+      transform: translate(-50%, -0%);
+    }
+  `
+);
+
 function CenterContainer(props) {
-  const { children, className } = props;
+  const { children, className, position = "" } = props;
 
   const nProps = Object.assign({}, props);
   delete nProps.className;
   delete nProps.children;
 
-  const centerClass = classNames(centerContainerClass, className);
+  const xy = position !== "x" && position !== "y";
+  const centerClass = classNames(
+    position === "x" && centerXContainerClass,
+    position === "y" && centerYContainerClass,
+    xy && `${centerContainerClass}`,
+    className
+  );
 
   return (
     <div className={centerClass} {...nProps}>
