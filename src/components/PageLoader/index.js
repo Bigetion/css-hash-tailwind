5691d3004e1e93286c5ac81845646504029d18a6
Bigetion <adysuprianto@yahoo.co.id>
2020-09-29 17:48:11 +0700

    Add Content Loader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { BulletList } from "react-content-loader";
 
 export default function PageLoader() {
   return (
     <div className="mb-6 px-6 max-w-3xl mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
-      <div className="flex items-center">
-        <h1 className="text-3xl">Loading content...</h1>
-      </div>
-      <p>Please wait</p>
+      <BulletList
+        backgroundColor="#CBD5E0"
+        foregroundColor="#EDF2F7"
+        style={{ width: "100%" }}
+      />
     </div>
   );
 }
