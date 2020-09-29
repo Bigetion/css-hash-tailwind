465c3a45fc0e7e49bb32479255296c4b4fee537a
Bigetion <adysuprianto@yahoo.co.id>
2020-09-29 18:05:39 +0700

    Update PageLoader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -4,7 +4,10 @@ export default function PageLoader() {
   return (
     <div className="mb-6 px-6 max-w-3xl mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
       <div className="flex items-center">
-        <h1 className="text-3xl">Loading content...</h1>
+        <h1 className="text-3xl">.....</h1>
+      </div>
+      <div className="mt-4">
+        <h1 className="text-xl">..........</h1>
       </div>
     </div>
   );
