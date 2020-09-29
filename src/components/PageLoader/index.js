5127653fb801a5f16091faa2cbf741d5e5e3aee5
Bigetion <adysuprianto@yahoo.co.id>
2020-09-29 17:52:09 +0700

    Remove Content Loader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { BulletList } from "react-content-loader";
 
 export default function PageLoader() {
   return (
     <div className="mb-6 px-6 max-w-3xl mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
-      <BulletList
-        backgroundColor="#CBD5E0"
-        foregroundColor="#EDF2F7"
-        style={{ width: "100%" }}
-      />
+      <div className="flex items-center">
+        <h1 className="text-3xl">Loading content...</h1>
+      </div>
     </div>
   );
 }
