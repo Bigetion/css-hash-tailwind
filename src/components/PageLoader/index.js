db677864cff66bae59009a05f7a96e7478b40c39
Bigetion <adysuprianto@yahoo.co.id>
2020-09-29 18:53:17 +0700

    Update PageLoader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -1,41 +1,27 @@
 import React from "react";
 
+function ContentLoader() {
+  return (
+    <div className="mt-4">
+      <h1 className="text-xl text-gray-700 mb-2">.....</h1>
+      <div className="rounded-lg overflow-hidden border border-gray-400 p-4">
+        <div className="space-y-6">
+          <div className="text-gray-700 bg-gray-400 h-5"></div>
+          <div className="text-gray-700 bg-gray-400 h-5"></div>
+          <div className="text-gray-700 bg-gray-400 h-5"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default function PageLoader() {
   return (
     <div className="mb-6 px-6 max-w-3xl mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
       <div className="flex items-center">
-        <h1 className="text-3xl">.....</h1>
+        <h1 className="text-3xl">..........</h1>
       </div>
-      <div className="mt-4">
-        <h1 className="text-xl text-gray-700 mb-2">..........</h1>
-        <div className="rounded-lg overflow-hidden border border-gray-400 p-4">
-          <div className="space-y-6">
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-4">
-        <h1 className="text-xl text-gray-700 mb-2">..........</h1>
-        <div className="rounded-lg overflow-hidden border border-gray-400 p-4">
-          <div className="space-y-6">
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-4">
-        <h1 className="text-xl text-gray-700 mb-2">..........</h1>
-        <div className="rounded-lg overflow-hidden border border-gray-400 p-4">
-          <div className="space-y-6">
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-            <div className="text-gray-700 bg-gray-400 h-5"></div>
-          </div>
-        </div>
-      </div>
+      <ContentLoader />
+      <ContentLoader />
     </div>
   );
 }
