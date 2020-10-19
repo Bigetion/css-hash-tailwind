d8ba73e2aceb06e4974cd1a65fb06a09b8f83fb0
Bigetion <adysuprianto@yahoo.co.id>
2020-10-19 08:03:22 +0800

    Update Version

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -67,7 +67,7 @@ export default function Home() {
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <a
-                        href="#"
+                        href="/"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
@@ -113,7 +113,7 @@ export default function Home() {
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <a
-                        href="#"
+                        href="/"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
@@ -159,7 +159,7 @@ export default function Home() {
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <a
-                        href="#"
+                        href="/"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
@@ -205,7 +205,7 @@ export default function Home() {
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <a
-                        href="#"
+                        href="/"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
@@ -251,7 +251,7 @@ export default function Home() {
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <a
-                        href="#"
+                        href="/"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
@@ -297,7 +297,7 @@ export default function Home() {
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <a
-                        href="#"
+                        href="/"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
@@ -313,13 +313,13 @@ export default function Home() {
       <div className="bg-white flex items-center justify-between">
         <div className="flex-1 flex justify-between sm:hidden">
           <a
-            href="#"
+            href="/"
             className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:shadow-outline-blue-300 focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
           >
             Previous
           </a>
           <a
-            href="#"
+            href="/"
             className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:shadow-outline-blue-300 focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
           >
             Next
@@ -336,7 +336,7 @@ export default function Home() {
           <div>
             <nav className="relative z-0 inline-flex shadow-sm">
               <a
-                href="#"
+                href="/"
                 className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
                 aria-label="Previous"
               >
@@ -354,19 +354,19 @@ export default function Home() {
                 </svg>
               </a>
               <a
-                href="#"
+                href="/"
                 className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >
                 1
               </a>
               <a
-                href="#"
+                href="/"
                 className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >
                 2
               </a>
               <a
-                href="#"
+                href="/"
                 className="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >
                 3
@@ -375,25 +375,25 @@ export default function Home() {
                 ...
               </span>
               <a
-                href="#"
+                href="/"
                 className="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >
                 8
               </a>
               <a
-                href="#"
+                href="/"
                 className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >
                 9
               </a>
               <a
-                href="#"
+                href="/"
                 className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >
                 10
               </a>
               <a
-                href="#"
+                href="/"
                 className="-ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:shadow-outline-blue-300 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
                 aria-label="Next"
               >
