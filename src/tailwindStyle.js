408ba35e72634963b0368f2cd06261f1aa558135
Bigetion <adysuprianto@yahoo.co.id>
2021-02-11 21:15:56 +0800

    Update Tailwind Version

--- src/tailwindStyle.js
+++ src/tailwindStyle.js
@@ -1,7 +1,5 @@
 import { cssHash } from "css-hash";
 
-import generateTailwindCss from "tailwind-css-generator";
+import { generateAll } from "tailwind-css-generator";
 
-const cssString = generateTailwindCss();
-
-cssHash(() => cssString);
+cssHash(() => generateAll());
