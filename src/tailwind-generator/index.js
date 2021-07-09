bdc9a676784e69e16ff17421bd222775cfb027b8
Bigetion <adysuprianto@yahoo.co.id>
2021-07-09 13:21:45 +0800

    Update Fix

--- src/tailwind-generator/index.js
+++ src/tailwind-generator/index.js
@@ -110,234 +110,234 @@ import generateStrokeWidth from "./generators/strokeWidth";
 import generateAccessibility from "./generators/accessibility";
 
 export function generateTailwindCssString(options = {}) {
-	const configOptions = getConfigOptions(options);
-	const { corePlugins = {} } = configOptions;
-	let cssString = "";
-	cssString += corePlugins.base ? generateBase(configOptions) : "";
-	cssString += corePlugins.form ? generateForm(configOptions) : "";
-	cssString += corePlugins.transform ? generateTransform(configOptions) : "";
-	cssString += corePlugins.container ? generateContainer(configOptions) : "";
-	cssString += corePlugins.boxSizing ? generateBoxSizing(configOptions) : "";
-	cssString += corePlugins.display ? generateDisplay(configOptions) : "";
-	cssString += corePlugins.float ? generateFloat(configOptions) : "";
-	cssString += corePlugins.clear ? generateClear(configOptions) : "";
-	cssString += corePlugins.objectFit ? generateObjectFit(configOptions) : "";
-	cssString += corePlugins.objectPosition
-		? generateObjectPosition(configOptions)
-		: "";
-	cssString += corePlugins.overflow ? generateOverflow(configOptions) : "";
-	cssString += corePlugins.overscrollBehavior
-		? generateOverscrollBehavior(configOptions)
-		: "";
-	cssString += corePlugins.position ? generatePosition(configOptions) : "";
-	cssString += corePlugins.inset ? generateInset(configOptions) : "";
-	cssString += corePlugins.visibility ? generateVisibility(configOptions) : "";
-	cssString += corePlugins.zIndex ? generateZIndex(configOptions) : "";
-	cssString += corePlugins.flexDirection
-		? generateFlexDirection(configOptions)
-		: "";
-	cssString += corePlugins.flexWrap ? generateFlexWrap(configOptions) : "";
-	cssString += corePlugins.flex ? generateFlex(configOptions) : "";
-	cssString += corePlugins.flexGrow ? generateFlexGrow(configOptions) : "";
-	cssString += corePlugins.flexShrink ? generateFlexShrink(configOptions) : "";
-	cssString += corePlugins.order ? generateOrder(configOptions) : "";
-	cssString += corePlugins.gridTemplateColumns
-		? generateGridTemplateColumns(configOptions)
-		: "";
-	cssString += corePlugins.gridColumn ? generateGridColumn(configOptions) : "";
-	cssString += corePlugins.gridColumnStart
-		? generateGridColumnStart(configOptions)
-		: "";
-	cssString += corePlugins.gridColumnEnd
-		? generateGridColumnEnd(configOptions)
-		: "";
-	cssString += corePlugins.gridTemplateRows
-		? generateGridTemplateRows(configOptions)
-		: "";
-	cssString += corePlugins.gridRow ? generateGridRow(configOptions) : "";
-	cssString += corePlugins.gridRowStart
-		? generateGridRowStart(configOptions)
-		: "";
-	cssString += corePlugins.gridRowEnd ? generateGridRowEnd(configOptions) : "";
-	cssString += corePlugins.gridAutoFlow
-		? generateGridAutoFlow(configOptions)
-		: "";
-	cssString += corePlugins.gridAutoColumns
-		? generateGridAutoColumns(configOptions)
-		: "";
-	cssString += corePlugins.gridAutoRows
-		? generateGridAutoRows(configOptions)
-		: "";
-	cssString += corePlugins.gap ? generateGap(configOptions) : "";
-	cssString += corePlugins.justifyContent
-		? generateJustifyContent(configOptions)
-		: "";
-	cssString += corePlugins.justifyItems
-		? generateJustifyItems(configOptions)
-		: "";
-	cssString += corePlugins.justifySelf
-		? generateJustifySelf(configOptions)
-		: "";
-	cssString += corePlugins.alignContent
-		? generateAlignContent(configOptions)
-		: "";
-	cssString += corePlugins.alignItems ? generateAlignItems(configOptions) : "";
-	cssString += corePlugins.alignSelf ? generateAlignSelf(configOptions) : "";
-	cssString += corePlugins.placeContent
-		? generatePlaceContent(configOptions)
-		: "";
-	cssString += corePlugins.placeItems ? generatePlaceItems(configOptions) : "";
-	cssString += corePlugins.placeSelf ? generatePlaceSelf(configOptions) : "";
-	cssString += corePlugins.padding ? generatePadding(configOptions) : "";
-	cssString += corePlugins.margin ? generateMargin(configOptions) : "";
-	cssString += corePlugins.space ? generateSpace(configOptions) : "";
-	cssString += corePlugins.width ? generateWidth(configOptions) : "";
-	cssString += corePlugins.minWidth ? generateMinWidth(configOptions) : "";
-	cssString += corePlugins.maxWidth ? generateMaxWidth(configOptions) : "";
-	cssString += corePlugins.height ? generateHeight(configOptions) : "";
-	cssString += corePlugins.minHeight ? generateMinHeight(configOptions) : "";
-	cssString += corePlugins.maxHeight ? generateMaxHeight(configOptions) : "";
-	cssString += corePlugins.fontSize ? generateFontSize(configOptions) : "";
-	cssString += corePlugins.fontSmoothing
-		? generateFontSmoothing(configOptions)
-		: "";
-	cssString += corePlugins.fontStyle ? generateFontStyle(configOptions) : "";
-	cssString += corePlugins.fontWeight ? generateFontWeight(configOptions) : "";
-	cssString += corePlugins.fontVariantNumeric
-		? generateFontVariantNumeric(configOptions)
-		: "";
-	cssString += corePlugins.letterSpacing
-		? generateLetterSpacing(configOptions)
-		: "";
-	cssString += corePlugins.lineHeight ? generateLineHeight(configOptions) : "";
-	cssString += corePlugins.listStyleType
-		? generateListStyleType(configOptions)
-		: "";
-	cssString += corePlugins.listStylePosition
-		? generateListStylePosition(configOptions)
-		: "";
-	cssString += corePlugins.placeholderColor
-		? generatePlaceholderColor(configOptions)
-		: "";
-	cssString += corePlugins.placeholderOpacity
-		? generatePlaceholderOpacity(configOptions)
-		: "";
-	cssString += corePlugins.textAlign ? generateTextAlign(configOptions) : "";
-	cssString += corePlugins.textColor ? generateTextColor(configOptions) : "";
-	cssString += corePlugins.textOpacity
-		? generateTextOpacity(configOptions)
-		: "";
-	cssString += corePlugins.textDecoration
-		? generateTextDecoration(configOptions)
-		: "";
-	cssString += corePlugins.textTransform
-		? generateTextTransform(configOptions)
-		: "";
-	cssString += corePlugins.verticalAlign
-		? generateVerticalAlign(configOptions)
-		: "";
-	cssString += corePlugins.whitespace ? generateWhitespace(configOptions) : "";
-	cssString += corePlugins.wordBreak ? generateWordBreak(configOptions) : "";
-	cssString += corePlugins.backgroundAttachment
-		? generateBackgroundAttachment(configOptions)
-		: "";
-	cssString += corePlugins.backgroundClip
-		? generateBackgroundClip(configOptions)
-		: "";
-	cssString += corePlugins.backgroundColor
-		? generateBackgroundColor(configOptions)
-		: "";
-	cssString += corePlugins.backgroundOpacity
-		? generateBackgroundOpacity(configOptions)
-		: "";
-	cssString += corePlugins.backgroundPosition
-		? generateBackgroundPosition(configOptions)
-		: "";
-	cssString += corePlugins.backgroundRepeat
-		? generateBackgroundRepeat(configOptions)
-		: "";
-	cssString += corePlugins.backgroundSize
-		? generateBackgroundSize(configOptions)
-		: "";
-	cssString += corePlugins.backgroundImage
-		? generateBackgroundImage(configOptions)
-		: "";
-	cssString += corePlugins.gradientColorStops
-		? generateGradientColorStops(configOptions)
-		: "";
-	cssString += corePlugins.borderRadius
-		? generateBorderRadius(configOptions)
-		: "";
-	cssString += corePlugins.borderWidth
-		? generateBorderWidth(configOptions)
-		: "";
-	cssString += corePlugins.borderColor
-		? generateBorderColor(configOptions)
-		: "";
-	cssString += corePlugins.borderOpacity
-		? generateBorderOpacity(configOptions)
-		: "";
-	cssString += corePlugins.borderStyle
-		? generateBorderStyle(configOptions)
-		: "";
-	cssString += corePlugins.divideWidth
-		? generateDivideWidth(configOptions)
-		: "";
-	cssString += corePlugins.divideColor
-		? generateDivideColor(configOptions)
-		: "";
-	cssString += corePlugins.divideOpacity
-		? generateDivideOpacity(configOptions)
-		: "";
-	cssString += corePlugins.divideStyle
-		? generateDivideStyle(configOptions)
-		: "";
-	cssString += corePlugins.borderCollapse
-		? generateBorderCollapse(configOptions)
-		: "";
-	cssString += corePlugins.tableLayout
-		? generateTableLayout(configOptions)
-		: "";
-	cssString += corePlugins.boxShadow ? generateBoxShadow(configOptions) : "";
-	cssString += corePlugins.opacity ? generateOpacity(configOptions) : "";
-	cssString += corePlugins.scale ? generateScale(configOptions) : "";
-	cssString += corePlugins.rotate ? generateRotate(configOptions) : "";
-	cssString += corePlugins.translate ? generateTranslate(configOptions) : "";
-	cssString += corePlugins.skew ? generateSkew(configOptions) : "";
-	cssString += corePlugins.transformOrigin
-		? generateTransformOrigin(configOptions)
-		: "";
-	cssString += corePlugins.appearance ? generateAppearance(configOptions) : "";
-	cssString += corePlugins.cursor ? generateCursor(configOptions) : "";
-	cssString += corePlugins.outline ? generateOutline(configOptions) : "";
-	cssString += corePlugins.pointerEvents
-		? generatePointerEvents(configOptions)
-		: "";
-	cssString += corePlugins.resize ? generateResize(configOptions) : "";
-	cssString += corePlugins.userSelect ? generateUserSelect(configOptions) : "";
-	cssString += corePlugins.fill ? generateFill(configOptions) : "";
-	cssString += corePlugins.stroke ? generateStroke(configOptions) : "";
-	cssString += corePlugins.strokeWidth
-		? generateStrokeWidth(configOptions)
-		: "";
-	cssString += corePlugins.accessibility
-		? generateAccessibility(configOptions)
-		: "";
+  const configOptions = getConfigOptions(options);
+  const { corePlugins = {} } = configOptions;
+  let cssString = "";
+  cssString += corePlugins.base ? generateBase(configOptions) : "";
+  cssString += corePlugins.form ? generateForm(configOptions) : "";
+  cssString += corePlugins.transform ? generateTransform(configOptions) : "";
+  cssString += corePlugins.container ? generateContainer(configOptions) : "";
+  cssString += corePlugins.boxSizing ? generateBoxSizing(configOptions) : "";
+  cssString += corePlugins.display ? generateDisplay(configOptions) : "";
+  cssString += corePlugins.float ? generateFloat(configOptions) : "";
+  cssString += corePlugins.clear ? generateClear(configOptions) : "";
+  cssString += corePlugins.objectFit ? generateObjectFit(configOptions) : "";
+  cssString += corePlugins.objectPosition
+    ? generateObjectPosition(configOptions)
+    : "";
+  cssString += corePlugins.overflow ? generateOverflow(configOptions) : "";
+  cssString += corePlugins.overscrollBehavior
+    ? generateOverscrollBehavior(configOptions)
+    : "";
+  cssString += corePlugins.position ? generatePosition(configOptions) : "";
+  cssString += corePlugins.inset ? generateInset(configOptions) : "";
+  cssString += corePlugins.visibility ? generateVisibility(configOptions) : "";
+  cssString += corePlugins.zIndex ? generateZIndex(configOptions) : "";
+  cssString += corePlugins.flexDirection
+    ? generateFlexDirection(configOptions)
+    : "";
+  cssString += corePlugins.flexWrap ? generateFlexWrap(configOptions) : "";
+  cssString += corePlugins.flex ? generateFlex(configOptions) : "";
+  cssString += corePlugins.flexGrow ? generateFlexGrow(configOptions) : "";
+  cssString += corePlugins.flexShrink ? generateFlexShrink(configOptions) : "";
+  cssString += corePlugins.order ? generateOrder(configOptions) : "";
+  cssString += corePlugins.gridTemplateColumns
+    ? generateGridTemplateColumns(configOptions)
+    : "";
+  cssString += corePlugins.gridColumn ? generateGridColumn(configOptions) : "";
+  cssString += corePlugins.gridColumnStart
+    ? generateGridColumnStart(configOptions)
+    : "";
+  cssString += corePlugins.gridColumnEnd
+    ? generateGridColumnEnd(configOptions)
+    : "";
+  cssString += corePlugins.gridTemplateRows
+    ? generateGridTemplateRows(configOptions)
+    : "";
+  cssString += corePlugins.gridRow ? generateGridRow(configOptions) : "";
+  cssString += corePlugins.gridRowStart
+    ? generateGridRowStart(configOptions)
+    : "";
+  cssString += corePlugins.gridRowEnd ? generateGridRowEnd(configOptions) : "";
+  cssString += corePlugins.gridAutoFlow
+    ? generateGridAutoFlow(configOptions)
+    : "";
+  cssString += corePlugins.gridAutoColumns
+    ? generateGridAutoColumns(configOptions)
+    : "";
+  cssString += corePlugins.gridAutoRows
+    ? generateGridAutoRows(configOptions)
+    : "";
+  cssString += corePlugins.gap ? generateGap(configOptions) : "";
+  cssString += corePlugins.justifyContent
+    ? generateJustifyContent(configOptions)
+    : "";
+  cssString += corePlugins.justifyItems
+    ? generateJustifyItems(configOptions)
+    : "";
+  cssString += corePlugins.justifySelf
+    ? generateJustifySelf(configOptions)
+    : "";
+  cssString += corePlugins.alignContent
+    ? generateAlignContent(configOptions)
+    : "";
+  cssString += corePlugins.alignItems ? generateAlignItems(configOptions) : "";
+  cssString += corePlugins.alignSelf ? generateAlignSelf(configOptions) : "";
+  cssString += corePlugins.placeContent
+    ? generatePlaceContent(configOptions)
+    : "";
+  cssString += corePlugins.placeItems ? generatePlaceItems(configOptions) : "";
+  cssString += corePlugins.placeSelf ? generatePlaceSelf(configOptions) : "";
+  cssString += corePlugins.padding ? generatePadding(configOptions) : "";
+  cssString += corePlugins.margin ? generateMargin(configOptions) : "";
+  cssString += corePlugins.space ? generateSpace(configOptions) : "";
+  cssString += corePlugins.width ? generateWidth(configOptions) : "";
+  cssString += corePlugins.minWidth ? generateMinWidth(configOptions) : "";
+  cssString += corePlugins.maxWidth ? generateMaxWidth(configOptions) : "";
+  cssString += corePlugins.height ? generateHeight(configOptions) : "";
+  cssString += corePlugins.minHeight ? generateMinHeight(configOptions) : "";
+  cssString += corePlugins.maxHeight ? generateMaxHeight(configOptions) : "";
+  cssString += corePlugins.fontSize ? generateFontSize(configOptions) : "";
+  cssString += corePlugins.fontSmoothing
+    ? generateFontSmoothing(configOptions)
+    : "";
+  cssString += corePlugins.fontStyle ? generateFontStyle(configOptions) : "";
+  cssString += corePlugins.fontWeight ? generateFontWeight(configOptions) : "";
+  cssString += corePlugins.fontVariantNumeric
+    ? generateFontVariantNumeric(configOptions)
+    : "";
+  cssString += corePlugins.letterSpacing
+    ? generateLetterSpacing(configOptions)
+    : "";
+  cssString += corePlugins.lineHeight ? generateLineHeight(configOptions) : "";
+  cssString += corePlugins.listStyleType
+    ? generateListStyleType(configOptions)
+    : "";
+  cssString += corePlugins.listStylePosition
+    ? generateListStylePosition(configOptions)
+    : "";
+  cssString += corePlugins.placeholderColor
+    ? generatePlaceholderColor(configOptions)
+    : "";
+  cssString += corePlugins.placeholderOpacity
+    ? generatePlaceholderOpacity(configOptions)
+    : "";
+  cssString += corePlugins.textAlign ? generateTextAlign(configOptions) : "";
+  cssString += corePlugins.textColor ? generateTextColor(configOptions) : "";
+  cssString += corePlugins.textOpacity
+    ? generateTextOpacity(configOptions)
+    : "";
+  cssString += corePlugins.textDecoration
+    ? generateTextDecoration(configOptions)
+    : "";
+  cssString += corePlugins.textTransform
+    ? generateTextTransform(configOptions)
+    : "";
+  cssString += corePlugins.verticalAlign
+    ? generateVerticalAlign(configOptions)
+    : "";
+  cssString += corePlugins.whitespace ? generateWhitespace(configOptions) : "";
+  cssString += corePlugins.wordBreak ? generateWordBreak(configOptions) : "";
+  cssString += corePlugins.backgroundAttachment
+    ? generateBackgroundAttachment(configOptions)
+    : "";
+  cssString += corePlugins.backgroundClip
+    ? generateBackgroundClip(configOptions)
+    : "";
+  cssString += corePlugins.backgroundColor
+    ? generateBackgroundColor(configOptions)
+    : "";
+  cssString += corePlugins.backgroundOpacity
+    ? generateBackgroundOpacity(configOptions)
+    : "";
+  cssString += corePlugins.backgroundPosition
+    ? generateBackgroundPosition(configOptions)
+    : "";
+  cssString += corePlugins.backgroundRepeat
+    ? generateBackgroundRepeat(configOptions)
+    : "";
+  cssString += corePlugins.backgroundSize
+    ? generateBackgroundSize(configOptions)
+    : "";
+  cssString += corePlugins.backgroundImage
+    ? generateBackgroundImage(configOptions)
+    : "";
+  cssString += corePlugins.gradientColorStops
+    ? generateGradientColorStops(configOptions)
+    : "";
+  cssString += corePlugins.borderRadius
+    ? generateBorderRadius(configOptions)
+    : "";
+  cssString += corePlugins.borderWidth
+    ? generateBorderWidth(configOptions)
+    : "";
+  cssString += corePlugins.borderColor
+    ? generateBorderColor(configOptions)
+    : "";
+  cssString += corePlugins.borderOpacity
+    ? generateBorderOpacity(configOptions)
+    : "";
+  cssString += corePlugins.borderStyle
+    ? generateBorderStyle(configOptions)
+    : "";
+  cssString += corePlugins.divideWidth
+    ? generateDivideWidth(configOptions)
+    : "";
+  cssString += corePlugins.divideColor
+    ? generateDivideColor(configOptions)
+    : "";
+  cssString += corePlugins.divideOpacity
+    ? generateDivideOpacity(configOptions)
+    : "";
+  cssString += corePlugins.divideStyle
+    ? generateDivideStyle(configOptions)
+    : "";
+  cssString += corePlugins.borderCollapse
+    ? generateBorderCollapse(configOptions)
+    : "";
+  cssString += corePlugins.tableLayout
+    ? generateTableLayout(configOptions)
+    : "";
+  cssString += corePlugins.boxShadow ? generateBoxShadow(configOptions) : "";
+  cssString += corePlugins.opacity ? generateOpacity(configOptions) : "";
+  cssString += corePlugins.scale ? generateScale(configOptions) : "";
+  cssString += corePlugins.rotate ? generateRotate(configOptions) : "";
+  cssString += corePlugins.translate ? generateTranslate(configOptions) : "";
+  cssString += corePlugins.skew ? generateSkew(configOptions) : "";
+  cssString += corePlugins.transformOrigin
+    ? generateTransformOrigin(configOptions)
+    : "";
+  cssString += corePlugins.appearance ? generateAppearance(configOptions) : "";
+  cssString += corePlugins.cursor ? generateCursor(configOptions) : "";
+  cssString += corePlugins.outline ? generateOutline(configOptions) : "";
+  cssString += corePlugins.pointerEvents
+    ? generatePointerEvents(configOptions)
+    : "";
+  cssString += corePlugins.resize ? generateResize(configOptions) : "";
+  cssString += corePlugins.userSelect ? generateUserSelect(configOptions) : "";
+  cssString += corePlugins.fill ? generateFill(configOptions) : "";
+  cssString += corePlugins.stroke ? generateStroke(configOptions) : "";
+  cssString += corePlugins.strokeWidth
+    ? generateStrokeWidth(configOptions)
+    : "";
+  cssString += corePlugins.accessibility
+    ? generateAccessibility(configOptions)
+    : "";
   return cssString;
 }
 
 function generateTailwindCss(options = {}) {
-  const cssString = generateTailwindCssString(options).replace(/\s\s+/g, ' ');
-	const head = document.head || document.getElementsByTagName("head")[0];
-	const style = document.createElement("style");
-	head.appendChild(style);
-	style.setAttribute("type", "text/css");
-	style.setAttribute("data-inline-style", "tailwind-css");
-	if (style.styleSheet) {
-		style.styleSheet.cssText = cssString;
-	} else {
-		style.appendChild(document.createTextNode(cssString));
-	}
+  const cssString = generateTailwindCssString(options).replace(/\s\s+/g, " ");
+  const head = document.head || document.getElementsByTagName("head")[0];
+  const style = document.createElement("style");
+  head.appendChild(style);
+  style.setAttribute("type", "text/css");
+  style.setAttribute("data-inline-style", "tailwind-css");
+  if (style.styleSheet) {
+    style.styleSheet.cssText = cssString;
+  } else {
+    style.appendChild(document.createTextNode(cssString));
+  }
 }
 
 export default generateTailwindCss;
