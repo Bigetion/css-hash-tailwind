79c0504756dcf410c1682f532ed8cc39e3293fa6
Bigetion <adysuprianto@yahoo.co.id>
2021-07-10 20:44:51 +0800

    Update fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Loadable from "react-loadable";
 
-import generateTailwindCss from "tailwind-css-generator";
+import generateTailwindCss from "tailwind-generator";
 import { cssHash } from "css-hash";
 
 import routes from "routes";
