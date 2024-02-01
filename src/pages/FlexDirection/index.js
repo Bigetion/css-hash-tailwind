0cd3541b3d9e1c6241ff7659ace10da24f818f53
Bigetion <adysuprianto@yahoo.co.id>
2024-02-01 10:32:26 +0700

    On Progress Flex

--- src/pages/FlexDirection/index.js
+++ src/pages/FlexDirection/index.js
@@ -3,71 +3,63 @@ import React from "react";
 import PanelSection from "components/PanelSection";
 
 export default function FlexDirection() {
-  return (
-    <div className="mb-6 px-6 mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
-      <div className="flex items-center">
-        <h1 className="text-3xl">Flex Direction</h1>
-      </div>
-      <PanelSection title="Row">
-        <div className="">
-          <div className="flex flex-row bg-gray-200">
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              1
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              2
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              3
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-      <PanelSection title="Row Reverse">
-        <div className="">
-          <div className="flex flex-row-reverse bg-gray-200">
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              1
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              2
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              3
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-      <PanelSection title="Column">
-        <div className="">
-          <div className="flex flex-col bg-gray-200">
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              1
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              2
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              3
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-      <PanelSection title="Column Reverse">
-        <div className="">
-          <div className="flex flex-col-reverse bg-gray-200">
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              1
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              2
-            </div>
-            <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-              3
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-    </div>
-  );
+	return (
+		<div className='mb-6 px-6 mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12'>
+			<div className='flex items-center'>
+				<h1 className='text-3xl'>Flex Direction</h1>
+			</div>
+			<PanelSection title='Row'>
+				<div className='flex flex-row gap-x-4 font-mono text-white text-sm font-bold leading-6'>
+					<div className='w-14 h-14 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+						01
+					</div>
+					<div className='w-14 h-14 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+						02
+					</div>
+					<div className='w-14 h-14 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+						03
+					</div>
+				</div>
+			</PanelSection>
+			<PanelSection title='Row Reverse'>
+				<div className='flex flex-row-reverse space-x-reverse gap-x-4 font-mono text-white text-sm font-bold leading-6'>
+					<div className='w-14 h-14 rounded-lg flex items-center justify-center bg-blue-500 shadow-lg'>
+						01
+					</div>
+					<div className='w-14 h-14 rounded-lg flex items-center justify-center bg-blue-500 shadow-lg'>
+						02
+					</div>
+					<div className='w-14 h-14 rounded-lg flex items-center justify-center bg-blue-500 shadow-lg'>
+						03
+					</div>
+				</div>
+			</PanelSection>
+			<PanelSection title='Column'>
+				<div className='flex flex-col mx-auto space-y-4 font-mono text-white text-sm font-bold leading-6 max-w-xs'>
+					<div className='p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg'>
+						01
+					</div>
+					<div className='p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg'>
+						02
+					</div>
+					<div className='p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg'>
+						03
+					</div>
+				</div>
+			</PanelSection>
+			<PanelSection title='Column Reverse'>
+				<div className='flex flex-col-reverse mx-auto space-y-reverse space-y-4 font-mono text-white text-sm font-bold leading-6 max-w-xs'>
+					<div className='p-4 rounded-lg flex items-center justify-center bg-purple-500 shadow-lg'>
+						01
+					</div>
+					<div className='p-4 rounded-lg flex items-center justify-center bg-purple-500 shadow-lg'>
+						02
+					</div>
+					<div className='p-4 rounded-lg flex items-center justify-center bg-purple-500 shadow-lg'>
+						03
+					</div>
+				</div>
+			</PanelSection>
+		</div>
+	);
 }
--- src/pages/FlexWrap/index.js
+++ src/pages/FlexWrap/index.js
@@ -1,58 +1,60 @@
 import React from "react";
 
+import { skyStripes, fuchsiaStripes } from 'utils/stripes';
+
 import PanelSection from "components/PanelSection";
 
 export default function FlexWrap() {
-  return (
-    <div className="mb-6 px-6 mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
-      <div className="flex items-center">
-        <h1 className="text-3xl">Flex Wrap</h1>
-      </div>
-      <PanelSection title="Don't Wrap">
-        <div className="">
-          <div className="flex flex-no-wrap bg-gray-200">
-            <div className="w-2/5 flex-none p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">1</div>
-            </div>
-            <div className="w-2/5 flex-none p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">2</div>
-            </div>
-            <div className="w-2/5 flex-none p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">3</div>
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-      <PanelSection title="Wrap">
-        <div className="">
-          <div className="flex flex-wrap bg-gray-200">
-            <div className="w-2/5 p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">1</div>
-            </div>
-            <div className="w-2/5 p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">2</div>
-            </div>
-            <div className="w-2/5 p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">3</div>
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-      <PanelSection title="Wrap Reversed">
-        <div className="">
-          <div className="flex flex-wrap-reverse bg-gray-200">
-            <div className="w-2/5 p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">1</div>
-            </div>
-            <div className="w-2/5 p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">2</div>
-            </div>
-            <div className="w-2/5 p-2">
-              <div className="text-gray-700 text-center bg-gray-400 p-2">3</div>
-            </div>
-          </div>
-        </div>
-      </PanelSection>
-    </div>
-  );
+	return (
+		<div className='mb-6 px-6 mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12'>
+			<div className='flex items-center'>
+				<h1 className='text-3xl'>Flex Wrap</h1>
+			</div>
+			<PanelSection title="Don't Wrap">
+				<div className={`flex flex-nowrap gap-4 font-mono text-white text-sm font-bold leading-6 bg-stripes-sky rounded-lg`}>
+					<div className='w-2/5 flex-none last:pr-8'>
+						<div className='p-4 w-full rounded-lg flex items-center justify-center bg-sky-500 shadow-lg'>
+							01
+						</div>
+					</div>
+					<div className='w-2/5 flex-none last:pr-8'>
+						<div className='p-4 w-full rounded-lg flex items-center justify-center bg-sky-500 shadow-lg'>
+							02
+						</div>
+					</div>
+					<div className='w-2/5 flex-none last:pr-8'>
+						<div className='p-4 w-full rounded-lg flex items-center justify-center bg-sky-500 shadow-lg'>
+							03
+						</div>
+					</div>
+				</div>
+			</PanelSection>
+			<PanelSection title='Wrap Normally'>
+				<div className='flex flex-wrap gap-4 font-mono text-white text-sm font-bold leading-6 bg-stripes-indigo rounded-lg'>
+					<div className='p-4 w-2/5 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg'>
+						01
+					</div>
+					<div className='p-4 w-2/5 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg'>
+						02
+					</div>
+					<div className='p-4 w-2/5 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg'>
+						03
+					</div>
+				</div>
+			</PanelSection>
+			<PanelSection title='Wrap Reversed'>
+				<div className='flex flex-wrap-reverse gap-4 font-mono text-white text-sm font-bold leading-6 bg-stripes-fuchsia rounded-lg'>
+					<div className='p-4 w-2/5 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+						01
+					</div>
+					<div className='p-4 w-2/5 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+						02
+					</div>
+					<div className='p-4 w-2/5 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+						03
+					</div>
+				</div>
+			</PanelSection>
+		</div>
+	);
 }
