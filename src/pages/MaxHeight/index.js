53d70db0238544c0ec6034caee044f967d4f35a8
Bigetion <adysuprianto@yahoo.co.id>
2024-02-05 08:20:32 +0700

    Fix Max Height Page

--- src/pages/MaxHeight/index.js
+++ src/pages/MaxHeight/index.js
@@ -11,65 +11,51 @@ export default function MaxHeight() {
         <h1 className="text-3xl">Max Height</h1>
       </div>
       <PanelSection title="Setting the maximum height">
-        <div className="h-96 flex justify-center items-end space-x-4 font-mono font-bold text-xs text-center text-white">
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-80 relative">
+        <div className="flex justify-center items-end gap-3 sm:gap-4 font-mono font-bold text-xs text-center text-white whitespace-nowrap h-96">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-80 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-80</div>
               </div>
             </div>
           </div>
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-64 relative">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-64 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-64</div>
               </div>
             </div>
           </div>
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-48 relative">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-48 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-48</div>
               </div>
             </div>
           </div>
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-40 relative">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-40 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-40</div>
               </div>
             </div>
           </div>
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-32 relative">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-32 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-32</div>
               </div>
             </div>
           </div>
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-24 relative">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-24 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-24</div>
               </div>
             </div>
           </div>
-          <div
-            className={`w-8 grid items-end ${stripes.blue} rounded-lg h-full`}
-          >
-            <div className="w-full bg-blue-500 rounded-lg shadow-lg h-full max-h-full relative">
+          <div className={`h-full rounded-lg flex items-end ${stripes.blue}`}>
+            <div className="w-8 bg-blue-500 rounded-lg shadow-lg h-full max-h-full h-96 relative">
               <div className="absolute w-8 bottom-6">
                 <div className="transform -rotate-90">max-h-full</div>
               </div>
