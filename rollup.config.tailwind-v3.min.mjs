17fbc251d8aa441bafdbfd32c9d522f0dad8ca95
Bigetion <adysuprianto@yahoo.co.id>
2024-03-01 11:26:24 +0700

    Update rollup config

--- rollup.config.tailwind-v3.min.mjs
+++ rollup.config.tailwind-v3.min.mjs
@@ -2,7 +2,7 @@ import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
 
 export default {
-	input: "src/tailwind_v3/index.js",
+	input: "src/tailwind_v3/index.min.js",
 	output: [
 		{
 			file: "dist/tailwind-css-generator.min.js",
--- rollup.config.tailwind-v3.mjs
+++ rollup.config.tailwind-v3.mjs
@@ -1,7 +1,7 @@
 import json from "@rollup/plugin-json";
 
 export default {
-	input: "src/tailwind_v3/index.min.js",
+	input: "src/tailwind_v3/index.js",
 	output: [
 		{
 			file: "dist/tailwind-css-generator.js",
