5315115c2a48ae8ca3d4f6111729f6429da76900
Bigetion <adysuprianto@yahoo.co.id>
2024-04-27 22:42:23 +0700

    Update PageLoader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -6,10 +6,10 @@ export default function PageLoader() {
     <div className="mb-6 px-6 mx-auto lg:ml-0 lg:mr-auto xl:mx-0 xl:px-12">
       <div className="w-full flex flex-col">
         <Shimmer>
-          <Shimmer.Item className="w-72 h-9 rounded mb-5" />
-          <Shimmer.Item className="w-96 h-6 rounded mb-2" />
+          <Shimmer.Item className="w-72 max-w-full h-9 rounded mb-5" />
+          <Shimmer.Item className="w-96 max-w-full h-6 rounded mb-2" />
           <Shimmer.Item className="w-full h-48 rounded mb-5" />
-          <Shimmer.Item className="w-96 h-6 rounded mb-2" />
+          <Shimmer.Item className="w-96 max-w-full h-6 rounded mb-2" />
           <Shimmer.Item className="w-full h-56 rounded mb-5" />
         </Shimmer>
       </div>
