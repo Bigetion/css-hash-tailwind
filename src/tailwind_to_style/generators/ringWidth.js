787e94aee84fda561fd78d0c1dd31c5cd0cf6e77
Bigetion <adysuprianto@yahoo.co.id>
2025-01-23 17:49:06 +0800

    Update ring default variable

--- src/tailwind_to_style/generators/ringWidth.js
+++ src/tailwind_to_style/generators/ringWidth.js
@@ -12,6 +12,10 @@ export default function generator(configOptions = {}) {
       const key = keyTmp.toLowerCase() !== "default" ? `-${keyTmp}` : "";
       return `
           ${prefix}${key} {
+            --ring-inset: var(--empty,/*!*/ /*!*/);
+            --ring-offset-width: 0px;
+            --ring-offset-color: #fff;
+            --ring-color: rgba(59, 130, 246, 0.5);
             --ring-offset-shadow: var(--ring-inset) 0 0 0 var(--ring-offset-width) var(--ring-offset-color);
             --ring-shadow: var(--ring-inset) 0 0 0 calc(${value} + var(--ring-offset-width)) var(--ring-color);
             box-shadow: var(--ring-offset-shadow), var(--ring-shadow);
