cde02d01a4d59b2dda32279dde1d78eb161f6f7f
Bigetion <shinichi.dconnan@gmail.com>
2025-04-11 10:07:06 +0800

    Update tailwind-to-style

--- rollup.config.tailwind-to-style.mjs
+++ rollup.config.tailwind-to-style.mjs
@@ -1,12 +1,22 @@
 import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
 
-export default {
-	input: "src/tailwind_to_style/index.js",
-	output: [
-		{
-			file: "dist/tailwind-to-style.js",
-			format: "cjs",
-		},
-	],
-	plugins: [json()],
-};
+export default [
+  {
+    input: "src/tailwind_to_style/index.js",
+    output: {
+      file: "dist/tailwind-to-style.js",
+      format: "cjs",
+    },
+    plugins: [json()],
+  },
+  {
+    input: "src/tailwind_to_style/index.min.js",
+    output: {
+      file: "dist/tailwind-to-style.min.js",
+      format: "umd",
+      name: "twsx",
+    },
+    plugins: [json(), terser()],
+  },
+];
--- rollup.config.tailwind-v3.mjs
+++ rollup.config.tailwind-v3.mjs
@@ -1,12 +1,22 @@
 import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
 
-export default {
-	input: "src/tailwind_v3/index.js",
-	output: [
-		{
-			file: "dist/tailwind-css-generator.js",
-			format: "cjs",
-		},
-	],
-	plugins: [json()],
-};
+export default [
+  {
+    input: "src/tailwind_v3/index.js",
+    output: {
+      file: "dist/tailwind-v3.js",
+      format: "cjs",
+    },
+    plugins: [json()],
+  },
+  {
+    input: "src/tailwind_v3/index.min.js",
+    output: {
+      file: "dist/tailwind-v3.min.js",
+      format: "umd",
+      name: "twsx",
+    },
+    plugins: [json(), terser()],
+  },
+];
