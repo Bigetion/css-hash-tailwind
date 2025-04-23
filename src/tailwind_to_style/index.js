d217010755c46bc7550ade14259d65f0045e9826
Bigetion <adysuprianto@yahoo.co.id>
2025-04-23 22:32:55 +0700

    Fix custom value with parens and close

--- src/tailwind_to_style/index.js
+++ src/tailwind_to_style/index.js
@@ -487,6 +487,21 @@ const selectorVariants = {
   number: (arg) => `> :nth-child(${arg})`,
 };
 
+function encodeBracketValues(input) {
+  return input.replace(/\[([^\]]+)\]/g, (_, content) => {
+    const encoded = encodeURIComponent(content)
+      .replace(/\(/g, "__P__")
+      .replace(/\)/g, "__C__");
+    return `[${encoded}]`;
+  });
+}
+
+function decodeBracketValues(input) {
+  return decodeURIComponent(input)
+    .replace(/__P__/g, "(")
+    .replace(/__C__/g, ")");
+}
+
 function replaceSelector(selector) {
   return selector.replace(
     /c-(first|last|odd|even|\d+|not\([^)]+\))/g,
@@ -568,7 +583,7 @@ function twsx(obj) {
       );
     }
 
-    let result = input;
+    let result = encodeBracketValues(input);
     let prev;
 
     do {
@@ -611,7 +626,10 @@ function twsx(obj) {
             const customKey = `${prefix}custom`;
             const template = cssObject[customKey];
             if (template) {
-              declarations = template.replace(/custom_value/g, dynamicValue);
+              declarations = template.replace(
+                /custom_value/g,
+                decodeBracketValues(dynamicValue)
+              );
             }
           }
         }
